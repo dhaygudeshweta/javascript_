807c21b5907f11804da34c104a297265641db4da
dhaygudeshweta <59594780+dhaygudeshweta@users.noreply.github.com>
2021-09-26 15:34:54 +0530

    Add files via upload

---
+++ ob.js
@@ -0,0 +1,24 @@
+// const myob={
+// name:"shweta",
+// lastname:"dhaygude",
+// age:"24",
+// adress:"barramti",
+// fullName : function() {
+//     return this.name + " " + this.lastname;
+//   }
+// };
+
+// console.log(myob.fullName());
+
+
+
+
+
+
+const person={}
+person.name="shweta",
+person.age="24"
+person.fullName=function (){
+    return person.name ;
+}
+console.log(person);
---
+++ obliteral.js
@@ -0,0 +1,48 @@
+// const person={
+// name:"shweta",
+// lastname:"dhaygude",
+// age:24
+// }
+
+// person.adress="baramati";
+// console.log(person);
+
+
+// function disaplaycities(){
+//     const array=['pune','mumbai','nashik'];
+    
+// return  array;
+// }
+// let displayHometownData={
+//     hometown:{
+//         city: "West Orange",
+//         state: "NJ",
+//     }
+   
+// }
+// console.log(displayHometownData.hometown);
+
+
+ let person ={
+    name:"shweta",
+
+     additionalData:{
+
+        lastname:"dhaygude",
+        age:24,
+
+        hometown:{
+            address:"baramti",
+            zipcode:413110
+        }
+
+     }
+
+ 
+
+ }
+
+console.log(person);
+
+
+ 
